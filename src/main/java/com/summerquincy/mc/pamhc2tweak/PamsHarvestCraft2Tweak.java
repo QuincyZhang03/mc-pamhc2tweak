e5089c98954269c3359194dc8bc027ffee47b82f
55021613E <1391070463@qq.com>
2026-08-26 04:06:12 +0800

    Port to NeoForge 1.21.1

---
+++ src/main/java/com/summerquincy/mc/pamhc2tweak/PamsHarvestCraft2Tweak.java
@@ -0,0 +1,41 @@
+package com.summerquincy.mc.pamhc2tweak;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(PamsHarvestCraft2Tweak.MODID)
+public class PamsHarvestCraft2Tweak {
+    public static final String MODID = "pamhc2tweak";
+    @SuppressWarnings("unused")
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+   @SuppressWarnings("unused")
+    public PamsHarvestCraft2Tweak(IEventBus modEventBus, ModContainer modContainer) {
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+}
